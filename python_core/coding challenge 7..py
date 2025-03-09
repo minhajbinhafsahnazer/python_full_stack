42f707a2b0aa7403654608a3d2166113657abb01
Minhaj pk <99128053+minhajbinhafsahnazer@users.noreply.github.com>
2025-03-10 01:41:46 +0530

    vnfe

---
+++ python_core/coding challenge 7..py
@@ -0,0 +1,75 @@
+#24.Write a Python program that uses list comprehension to create a new list containing the squares of the numbers from 1 to 10.
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#25.Write a Python program that uses a "for" loop to iterate over a string and prints out each character along with its count.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#26.Write a Python program that uses a list comprehension to create a new list that contains only the uppercase letters in an existing list of strings.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#27.To write a program in python find the 
+# second smallest and 
+# third largest number in a
+#  list.
+
+list1 = list(int(input("Enter the list of numbers : ")))
+
+
+
+
+
+
+
+
+
+
+
+
+
+#28.Write a Python program to remove duplicates from a list while preserving the order of elements.
+
+
