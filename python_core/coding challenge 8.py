c91c71f23f20cc81a2f0e81836d6ddba0a5d247d
Minhaj pk <99128053+minhajbinhafsahnazer@users.noreply.github.com>
2025-03-10 22:28:13 +0530

    vnfe

--- python_core/coding challenge 8.py
+++ python_core/coding challenge 8.py
@@ -1,3 +1,33 @@
 #29. Create a simple calculator operations  using function.
+'''
+num1 = int(input("Enter the Number 1 : "))
+operator = input("select calculation (+,-,*,/) : ")
+num2 = int(input("Enter the Number 2 : "))
+if operator == '+':
+    print(num1+num2)
+if operator == '-':
+    print(num1-num2)
+if operator == '*':
+    print(num1*num2)
+if operator == '/':
+    print(num1/num2)
+
+
+'''
+
 
 #30. Create a Python function that takes a string as input and counts the number of vowels and consonants in the string.
+vowels = 0
+consonents = 0
+string1 = input("Enter the string : ")
+vow = ("A","E","I","O","U","a","e","i","o","u")
+for i in string1:
+    if i.isalpha():
+        if i in vow:
+            vowels+=1
+        else:
+            consonents+=1
+print("Num of vowels =",vowels)
+print("Num of consonents =",consonents)
+
+
