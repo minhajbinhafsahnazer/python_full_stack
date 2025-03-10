8c5c94201bd7f2e71ffa7dc5a0986332b7fed995
Minhaj pk <99128053+minhajbinhafsahnazer@users.noreply.github.com>
2025-03-11 01:50:18 +0530

    vnfe

---
+++ python_core/coding challenge 31.py
@@ -0,0 +1,17 @@
+class cart():
+    def __init__(self,name,price,quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        
+    def add_product():
+        print(input("product name :"))
+
+    def rem_product():
+        del.product
+
+    def total_price(__init__):
+        total = self.price * self.quantity
+        print("total price :",total)
+
+    
