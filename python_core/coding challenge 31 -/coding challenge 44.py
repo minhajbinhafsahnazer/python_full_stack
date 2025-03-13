d3ce6b0d3fe54e9e9b74b29120d4a19e4e440145
Minhaj pk <99128053+minhajbinhafsahnazer@users.noreply.github.com>
2025-03-14 04:47:43 +0530

    vnfe

--- python_core/coding challenge 31 -/coding challenge 44.py
+++ python_core/coding challenge 31 -/coding challenge 44.py
@@ -1,1 +1,7 @@
-#Write a Python program to find numbers divisible by nineteen or thirteen from a list of numbers using Lambda.
+#Write a Python program to find numbers divisible by nineteen or thirteen from a list of numbers using Lambda.\
+
+numbers = [19, 26, 38, 52, 65, 76, 91, 114, 133, 152, 190, 247]
+
+divisible_numbers = list(filter(lambda x: x % 19 == 0 or x % 13 == 0, numbers))
+
+print("Numbers divisible by 19 or 13:", divisible_numbers)
--- python_core/coding challenge 31 -/coding challenge 45.py
+++ python_core/coding challenge 31 -/coding challenge 45.py
@@ -1,3 +1,9 @@
 # Write a Python program to calculate the sum of the positive and negative numbers of a given list of numbers using the lambda function
-#
-#
+
+numbers = [10, -5, 20, -15, 30, -25, 40, -35]
+
+positive_sum = sum(filter(lambda x: x > 0, numbers))
+negative_sum = sum(filter(lambda x: x < 0, numbers))
+
+print("Sum of positive numbers:", positive_sum)
+print("Sum of negative numbers:", negative_sum)
