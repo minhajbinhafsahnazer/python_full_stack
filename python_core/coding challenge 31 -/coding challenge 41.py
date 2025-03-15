d7b82880a91c579a10f6eaca35b14c4a184687eb
Minhaj pk <99128053+minhajbinhafsahnazer@users.noreply.github.com>
2025-03-16 02:02:31 +0530

    vnfe

--- python_core/coding challenge 31 -/coding challenge 41.py
+++ python_core/coding challenge 31 -/coding challenge 41.py
@@ -1,3 +1,4 @@
+#41
 phone = [{'make': 'Nokia', 'model': 216, 'color': 'Black'}, {'make': 'Mi Max', 'model': '2', 'color': 'Gold'}, {'make': 'Samsung', 'model': 7, 'color': 'Blue'}]
 
 sorted_list = sorted(phone,key = lambda x : int(x['model']))
